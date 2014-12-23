ee15d8f5b4b09c11edbcebd1f0d91c55fa33f37f
desperado-bvb <desperado.bvb@gmail.com>
2014-12-23 18:01:47 +0800

    add unsub

--- agent/http.go
+++ agent/http.go
@@ -1,197 +1,197 @@
 package agent
 
 import (
-    "errors"
-    "fmt"
-    "io"
-    "io/ioutil"
-    "net/http"
-    httpprof "net/http/pprof"
-    "net/url"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	httpprof "net/http/pprof"
+	"net/url"
 
-    "github.com/desperado-bvb/dortmund/util"
+	"github.com/desperado-bvb/dortmund/util"
 )
 
 type httpServer struct {
-    ctx  *context
+	ctx *context
 }
 
 func (s *httpServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
-    err := s.v1Router(w, req)
-    if err == nil {
-	return
-    }
+	err := s.v1Router(w, req)
+	if err == nil {
+		return
+	}
 
-    err = s.debugRouter(w, req)
-    if err != nil {
-	s.ctx.svr.logf("ERROR: %s", err)
-	util.ApiResponse(w, 404, "NOT_FOUND", nil)
-    }
+	err = s.debugRouter(w, req)
+	if err != nil {
+		s.ctx.svr.logf("ERROR: %s", err)
+		util.ApiResponse(w, 404, "NOT_FOUND", nil)
+	}
 }
 
 func (s *httpServer) debugRouter(w http.ResponseWriter, req *http.Request) error {
-    switch req.URL.Path {
-    case "/debug/pprof":
-	httpprof.Index(w, req)
-    case "/debug/pprof/cmdline":
-	httpprof.Cmdline(w, req)
-    case "/debug/pprof/symbol":
-	httpprof.Symbol(w, req)
-    case "/debug/pprof/heap":
-	httpprof.Handler("heap").ServeHTTP(w, req)
-    case "/debug/pprof/goroutine":
-	httpprof.Handler("goroutine").ServeHTTP(w, req)
-    case "/debug/pprof/profile":
-	httpprof.Profile(w, req)
-    case "/debug/pprof/block":
-	httpprof.Handler("block").ServeHTTP(w, req)
-    case "/debug/pprof/threadcreate":
-	httpprof.Handler("threadcreate").ServeHTTP(w, req)
-    default:
-	return errors.New(fmt.Sprintf("404 %s", req.URL.Path))
-    }
+	switch req.URL.Path {
+	case "/debug/pprof":
+		httpprof.Index(w, req)
+	case "/debug/pprof/cmdline":
+		httpprof.Cmdline(w, req)
+	case "/debug/pprof/symbol":
+		httpprof.Symbol(w, req)
+	case "/debug/pprof/heap":
+		httpprof.Handler("heap").ServeHTTP(w, req)
+	case "/debug/pprof/goroutine":
+		httpprof.Handler("goroutine").ServeHTTP(w, req)
+	case "/debug/pprof/profile":
+		httpprof.Profile(w, req)
+	case "/debug/pprof/block":
+		httpprof.Handler("block").ServeHTTP(w, req)
+	case "/debug/pprof/threadcreate":
+		httpprof.Handler("threadcreate").ServeHTTP(w, req)
+	default:
+		return errors.New(fmt.Sprintf("404 %s", req.URL.Path))
+	}
 
-    return nil
+	return nil
 }
 
 func (s *httpServer) v1Router(w http.ResponseWriter, req *http.Request) error {
-    switch req.URL.Path {
-    case "/pub":
-	util.NegotiateAPIResponseWrapper(w, req, util.POSTRequired(req,
-	     func() (interface{}, error) { return s.doPUB(req) }))
-     case "/sub":
-              util.NegotiateAPIResponseWrapper(w, req, util.POSTRequired(req,
-                     func() (interface{}, error) { return s.doSUB(req) }))
-    case "/sub":
-              util.NegotiateAPIResponseWrapper(w, req, util.POSTRequired(req,
-                     func() (interface{}, error) { return s.doUNSUB(req) }))
+	switch req.URL.Path {
+	case "/pub":
+		util.NegotiateAPIResponseWrapper(w, req, util.POSTRequired(req,
+			func() (interface{}, error) { return s.doPUB(req) }))
+	case "/sub":
+		util.NegotiateAPIResponseWrapper(w, req, util.POSTRequired(req,
+			func() (interface{}, error) { return s.doSUB(req) }))
+	case "/sub":
+		util.NegotiateAPIResponseWrapper(w, req, util.POSTRequired(req,
+			func() (interface{}, error) { return s.doUNSUB(req) }))
 
-    default:
-        return errors.New(fmt.Sprintf("404 %s", req.URL.Path))
-    }
-    return nil
+	default:
+		return errors.New(fmt.Sprintf("404 %s", req.URL.Path))
+	}
+	return nil
 }
 
 func (s *httpServer) doPUB(req *http.Request) (interface{}, error) {
 
-    if req.ContentLength > s.ctx.svr.opts.MaxMsgSize {
-	return nil, util.HTTPError{413, "MSG_TOO_BIG"}
-    }
+	if req.ContentLength > s.ctx.svr.opts.MaxMsgSize {
+		return nil, util.HTTPError{413, "MSG_TOO_BIG"}
+	}
 
-    readMax := s.ctx.svr.opts.MaxMsgSize + 1
-    body, err := ioutil.ReadAll(io.LimitReader(req.Body, readMax))
-    if err != nil {
-	return nil, util.HTTPError{500, "INTERNAL_ERROR"}
-    }
+	readMax := s.ctx.svr.opts.MaxMsgSize + 1
+	body, err := ioutil.ReadAll(io.LimitReader(req.Body, readMax))
+	if err != nil {
+		return nil, util.HTTPError{500, "INTERNAL_ERROR"}
+	}
 
-    if int64(len(body)) == readMax {
-	s.ctx.svr.logf("ERROR: /put hit max message size")
-	return nil, util.HTTPError{413, "MSG_TOO_BIG"}
-    }
+	if int64(len(body)) == readMax {
+		s.ctx.svr.logf("ERROR: /put hit max message size")
+		return nil, util.HTTPError{413, "MSG_TOO_BIG"}
+	}
 
-    if len(body) == 0 {
-	return nil, util.HTTPError{400, "MSG_EMPTY"}
-    }
+	if len(body) == 0 {
+		return nil, util.HTTPError{400, "MSG_EMPTY"}
+	}
 
-    topic, err := s.getTopicFromQuery(req)
-    if err != nil {
-	return nil, err
-    }
+	topic, err := s.getTopicFromQuery(req)
+	if err != nil {
+		return nil, err
+	}
 
-    fmt.Println(topic, string(body))
+	fmt.Println(topic, string(body))
 
-    r := s.ctx.svr.pubSvr.submitAsync(topic, body)
-    err = r.wait()
-    if err != nil {
-	return nil, util.HTTPError{503, "EXITING"}
-    }
+	r := s.ctx.svr.pubSvr.submitAsync(topic, body)
+	err = r.wait()
+	if err != nil {
+		return nil, util.HTTPError{503, "EXITING"}
+	}
 
-    return "OK", nil
+	return "OK", nil
 }
 
 func (s *httpServer) doSUB(req *http.Request) (interface{}, error) {
 
-    if req.ContentLength > s.ctx.svr.opts.MaxMsgSize {
-    return nil, util.HTTPError{413, "MSG_TOO_BIG"}
-    }
+	if req.ContentLength > s.ctx.svr.opts.MaxMsgSize {
+		return nil, util.HTTPError{413, "MSG_TOO_BIG"}
+	}
 
-    readMax := s.ctx.svr.opts.MaxMsgSize + 1
-    body, err := ioutil.ReadAll(io.LimitReader(req.Body, readMax))
-    if err != nil {
-    return nil, util.HTTPError{500, "INTERNAL_ERROR"}
-    }
+	readMax := s.ctx.svr.opts.MaxMsgSize + 1
+	body, err := ioutil.ReadAll(io.LimitReader(req.Body, readMax))
+	if err != nil {
+		return nil, util.HTTPError{500, "INTERNAL_ERROR"}
+	}
 
-    if int64(len(body)) == readMax {
-    s.ctx.svr.logf("ERROR: /put hit max message size")
-    return nil, util.HTTPError{413, "MSG_TOO_BIG"}
-    }
+	if int64(len(body)) == readMax {
+		s.ctx.svr.logf("ERROR: /put hit max message size")
+		return nil, util.HTTPError{413, "MSG_TOO_BIG"}
+	}
 
-    if len(body) == 0 {
-    return nil, util.HTTPError{400, "MSG_EMPTY"}
-    }
+	if len(body) == 0 {
+		return nil, util.HTTPError{400, "MSG_EMPTY"}
+	}
 
-    topic, err := s.getTopicFromQuery(req)
-    if err != nil {
-    return nil, err
-    }
-    
-    id, err := s.ctx.svr.createSub(topic, string(body))
-    if err != nil {
-    s.ctx.svr.logf("ERROR: create sub - %s", err)
-    return nil, util.HTTPError{503, "EXITING"}
-    }
+	topic, err := s.getTopicFromQuery(req)
+	if err != nil {
+		return nil, err
+	}
 
-    return id, nil
+	id, err := s.ctx.svr.createSub(topic, string(body))
+	if err != nil {
+		s.ctx.svr.logf("ERROR: create sub - %s", err)
+		return nil, util.HTTPError{503, "EXITING"}
+	}
+
+	return id, nil
 }
 
 func (s *httpServer) doUNSUB(req *http.Request) (interface{}, error) {
 
-    if req.ContentLength > s.ctx.svr.opts.MaxMsgSize {
-    return nil, util.HTTPError{413, "MSG_TOO_BIG"}
-    }
+	if req.ContentLength > s.ctx.svr.opts.MaxMsgSize {
+		return nil, util.HTTPError{413, "MSG_TOO_BIG"}
+	}
 
-    readMax := s.ctx.svr.opts.MaxMsgSize + 1
-    body, err := ioutil.ReadAll(io.LimitReader(req.Body, readMax))
-    if err != nil {
-    return nil, util.HTTPError{500, "INTERNAL_ERROR"}
-    }
+	readMax := s.ctx.svr.opts.MaxMsgSize + 1
+	body, err := ioutil.ReadAll(io.LimitReader(req.Body, readMax))
+	if err != nil {
+		return nil, util.HTTPError{500, "INTERNAL_ERROR"}
+	}
 
-    if int64(len(body)) == readMax {
-    s.ctx.svr.logf("ERROR: /put hit max message size")
-    return nil, util.HTTPError{413, "MSG_TOO_BIG"}
-    }
+	if int64(len(body)) == readMax {
+		s.ctx.svr.logf("ERROR: /put hit max message size")
+		return nil, util.HTTPError{413, "MSG_TOO_BIG"}
+	}
 
-    if len(body) == 0 {
-    return nil, util.HTTPError{400, "MSG_EMPTY"}
-    }
+	if len(body) == 0 {
+		return nil, util.HTTPError{400, "MSG_EMPTY"}
+	}
 
-    topic, err := s.getTopicFromQuery(req)
-    if err != nil {
-    return nil, err
-    }
-    
-    id, err := s.ctx.svr.DeleteExistingSub(string(body))
-    if err != nil {
-    s.ctx.svr.logf("ERROR: delete sub - %s", err)
-    return nil, util.HTTPError{503, "EXITING"}
-    }
+	topic, err := s.getTopicFromQuery(req)
+	if err != nil {
+		return nil, err
+	}
 
-    return "ok", nil
+	id, err := s.ctx.svr.DeleteExistingSub(string(body))
+	if err != nil {
+		s.ctx.svr.logf("ERROR: delete sub - %s", err)
+		return nil, util.HTTPError{503, "EXITING"}
+	}
+
+	return "OK", nil
 }
 
-func (s *httpServer) getTopicFromQuery(req *http.Request) (string,  error) {
-    reqParams, err := url.ParseQuery(req.URL.RawQuery)
-    if err != nil {
-	s.ctx.svr.logf("ERROR: failed to parse request params - %s", err)
-	return "",  util.HTTPError{400, "INVALID_REQUEST"}
-    }
+func (s *httpServer) getTopicFromQuery(req *http.Request) (string, error) {
+	reqParams, err := url.ParseQuery(req.URL.RawQuery)
+	if err != nil {
+		s.ctx.svr.logf("ERROR: failed to parse request params - %s", err)
+		return "", util.HTTPError{400, "INVALID_REQUEST"}
+	}
 
-    topicNames, ok := reqParams["topic"]
-    if !ok {
-	return "", util.HTTPError{400, "MISSING_ARG_TOPIC"}
-    }
-    topicName := topicNames[0]
+	topicNames, ok := reqParams["topic"]
+	if !ok {
+		return "", util.HTTPError{400, "MISSING_ARG_TOPIC"}
+	}
+	topicName := topicNames[0]
 
-    return topicName, nil
+	return topicName, nil
 }
